712f165432249a0baace4c6569b500d2dfaefa2d
ABCDEFGH <38960954+ABCDEFGHD@users.noreply.github.com>
2018-07-25 13:40:45 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const Client = new Discord.Client();
+
+const Ytdl = require("ytdl-core");
+const PREFIX = '=';
 
 Client.on("ready", () => {
   console.log("Logged in !");
 });
 
 Client.on("message", Message => {
+  const Content = Message.content;
 
+  if (Message.guild && Content.startsWith(PREFIX)) {
+    console.log(content);
+  }
 });
 
 Client.login(process.env.TOKEN);
