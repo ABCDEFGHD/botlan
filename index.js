6f95e06299e05ebfcdb6ba7a60860b296afd53b3
ABCDEFGH <38960954+ABCDEFGHD@users.noreply.github.com>
2018-07-25 13:47:38 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
 const Discord = require('discord.js');
-const Cleverbot = require("cleverbot-node");
-const clbot = new Cleverbot;
-const shorten = require('isgd');
-const ms = require('parse-ms');
-//const db = require('quick-db')
 
 var bot = new Discord.Client();
-var prefix = ("pb!");
+var prefix = ("lb!");
 var randum = 0;
-
 
 bot.on('ready', () => {
     bot.user.setPresence({ game: { name: `Manger du bambou | ${prefix}help | ${bot.guilds.size} serveurs`, type: 0}})
