05e29db07c8ea83be749fb304a6b4134df305c87
ABCDEFGH <38960954+ABCDEFGHD@users.noreply.github.com>
2018-07-25 10:05:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+const Discord = require('discord.js');
+const Cleverbot = require("cleverbot-node");
+const clbot = new Cleverbot;
+const shorten = require('isgd');
+const ms = require('parse-ms');
+//const db = require('quick-db')
 
+var bot = new Discord.Client();
+var prefix = ("lb!");
+var randum = 0;
+
+
+bot.on('ready', () => {
+    bot.user.setPresence({ game: { name: `Une lan | ${prefix}help`, type: 0}})
+    bot.user.setStatus("dnd");
+    console.log("Bot Prêt !");
+});
+
+bot.login(process.env.TOKEN)
